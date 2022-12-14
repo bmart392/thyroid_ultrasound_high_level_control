e83052c70eb5fcbf3e9c2ad475ae3b6b6712efc0
Ben Mart <bmart392@gmail.com>
2022-12-14 14:39:34 -0500

    Updated high level control to be purely logic and communication

--- scripts/HighLevelController.py
+++ scripts/HighLevelController.py
@@ -12,125 +12,88 @@ from motion_helpers import *
 
 class HighLevelController:
     def __init__(self) -> None:
+
         self.cartesian_position_history = []
-        self.image_centroid_error_history = []
-        self.external_force_history = []
-        self.thyroid_in_image_status = False
-        self.desired_end_effector_force = 0.1  # N
-        self.allowable_centroid_error = .1
-        self.acceptable_cartesian_error = .1
+        # self.image_centroid_error_history = []
+        # self.external_force_history = []
+        self.thyroid_in_image_status = True
+        self.is_thyroid_centered = False
+        self.was_last_goal_position_reached = False
+        # self.desired_end_effector_force = 0.1  # N
+        # self.allowable_centroid_error = .1
+        # self.acceptable_cartesian_error = .1
         self.standard_scan_step = array([0.01, 0.0, 0.0])
         self.move_goal = None
-        self.velocity_publisher = None
-        self.centroid_error_subscriber = None
-        self.thyroid_in_image_subscriber = None
-        self.robot_state_subscriber = None
-        self.force_readings_subscriber = None
+
+        # Command publishers
+        self.goal_pose_publisher: rospy.Publisher = None
+        self.desired_force_publisher: rospy.Publisher = None
+        self.stop_motion_publisher: rospy.Publisher = None
+        self.active_image_centering_status_publisher: rospy.Publisher = None
+
+        # Status subscribers
+        self.is_thyroid_in_image_status_subscriber: rospy.Subscriber = None
+        self.is_thyroid_centered_status_subscriber: rospy.Subscriber = None
+        self.was_last_goal_reached_status_subscriber: rospy.Subscriber = None
+
+        # self.velocity_publisher = None
+        # self.centroid_error_subscriber = None
+        # self.thyroid_in_image_subscriber = None
+        # self.robot_state_subscriber = None
+        # self.force_readings_subscriber = None
 
         # initialize ros node
         rospy.init_node('motion_controller')
 
         # create publishers and subscribers
-        self.init_publishers_and_subscirbers()
+        self.init_publishers_and_subscribers()
 
     # --------------------------------------------
     # Callback Functions
     # --------------------------------------------
 
-    """# capture the cartesian position of the robot
-    def cartesian_position_callback(data: FrankaState, self):
-
-        # limit the number of previous values stored to 5
-        if len(self.cartesian_position_history) >= 5:
-            self.cartesian_position_history.pop()
-
-        # add the new value to the list
-        self.cartesian_position_history.insert(0,
-                                               (data.header.stamp.sec,
-                                                array([data.O_T_EE[3],
-                                                       data.O_T_EE[7],
-                                                       data.O_T_EE[11]]))
-                                               )"""
-
-    # capture the error of the position of image centroid
-    def centroid_error_callback(data: Float64, self):
-
-        # remove oldest error value if more 5 have already been saved
-        if len(self.image_centroid_error_history) >= 5:
-            self.image_centroid_error_history.pop()
-
-        # save the new error value
-        self.image_centroid_error_history.append((0, array([0, data.data, 0])))
-
-    """# capture the current force felt by the robot
-    def force_value_callback(data: WrenchStamped, self):
-
-        # remove the oldest force value if more than 5 have already been saved
-        if len(self.external_force_history) >= 5:
-            self.external_force_history.pop()
-
-        # save the new error value
-        self.external_force_history.insert(
-            (data.head.time.sec, array([data.wrench.force.x, data.wrench.force.y, data.wrench.force.z])))"""
-
     # capture if the thyroid is in the current image
-    def thyroid_in_image_callback(data: Bool, self):
+    def is_thyroid_in_image_status_callback(self, data: Bool):
         self.thyroid_in_image_status = data.data
 
-    # --------------------------------------------
-    # Control Input Calculation Functions
-    # --------------------------------------------
+    # capture if the thyroid is centered in the image
+    def is_thyroid_centered_status_callback(self, data: Bool):
+        self.is_thyroid_centered = data.data
 
-    # calculate the control input based on the image error
-    def image_error_calculate_control_input(self):
-        k_p = .1
-        min_control_speed = 0.001  # m/s
-        max_control_speed = 0.025  # m/s
-        error = self.image_centroid_error_history[0]
-        return pd_controller(k_p, 0, error, 0, min_control_speed, max_control_speed)
-
-    # calculate the control input based on the force error
-    def force_error_calculate_control_input(self):
-        k_p = .1
-        k_d = 0
-        min_control_speed = .001  # m/s
-        max_control_speed = .025  # m/s
-        error, error_dot = calculate_error(self.desired_end_effector_force, self.external_force_history)
-        return pd_controller(k_p, k_d, error, error_dot, min_control_speed, max_control_speed)
-
-    # calculate the control input based on the cartesian position error
-    def cartesian_position_calculate_control_input(self):
-        k_p = .1
-        k_d = 0.
-        min_control_speed = .001  # m/s
-        max_control_speed = .025  # m/s
-        error, error_dot = calculate_error(self.move_goal, self.cartesian_position_history)
-        return pd_controller(k_p, k_d, error, error_dot, min_control_speed, max_control_speed)
+    # capture the status of the motion to reach the last sent goal point
+    def was_last_goal_reached_status_callback(self, data: Bool):
+        self.was_last_goal_position_reached = data.data
 
     # --------------------------------------------
     # Define ROS features
     # --------------------------------------------
 
     # create publisher and subscriber objects
-    def init_publishers_and_subscirbers(self):
-        # Create the publisher to publish the desired joint velocities
-        self.velocity_publisher = rospy.Publisher('/arm/cartesian/velocity', TwistStamped, queue_size=1)
+    def init_publishers_and_subscribers(self):
 
-        # Create a subscriber to listen to the error gathered from ultrasound images
-        self.centroid_error_subscriber = rospy.Subscriber('/image_data/centroid_error', Float64,
-                                                          self.centroid_error_callback)
+        # Create a publisher to publish goal positions
+        self.goal_pose_publisher = rospy.Publisher('/command/goal_pose', TwistStamped, queue_size=1)
 
-        # Create a subscriber to listen to the error gathered from ultrasound images
-        self.thyroid_in_image_subscriber = rospy.Subscriber('/image_data/thyroid_in_image', Bool,
-                                                            self.thyroid_in_image_callback)
+        # Create a publisher to publish the desired applied force
+        self.desired_force_publisher = rospy.Publisher('/command/desired_force', WrenchStamped, queue_size=1)
 
-        """# Create a subscriber to listen to the robot state
-        self.robot_state_subscriber = rospy.Subscriber('/franka_state_controller/franka_states', FrankaState,
-                                                       self.cartesian_position_callback)
+        # Create a publisher to publish a stop-motion command
+        self.stop_motion_publisher = rospy.Publisher('/command/stop_motion', Bool, queue_size=1)
 
-        # Create a subscriber to listen to the force readings from the robot
-        self.force_readings_subscriber = rospy.Subscriber('/franka_state_controller/F_ext', WrenchStamped,
-                                                          self.force_value_callback)"""
+        # Create a publisher to publish the status of the image centering motions
+        self.active_image_centering_status_publisher = rospy.Publisher('/command/center_image', Bool, queue_size=1)
+
+        # Create a subscriber to check if the thyroid is in the image
+        self.is_thyroid_in_image_status_subscriber = rospy.Subscriber('/status/thyroid_shown', Bool,
+                                                                      self.is_thyroid_in_image_status_callback)
+
+        # Create a subscriber to check if the image is centered
+        self.is_thyroid_centered_status_subscriber = rospy.Subscriber('/status/thyroid_centered', Bool,
+                                                                      self.is_thyroid_centered_status_callback)
+
+        # Create a subscriber to check if the last goal sent was reached
+        self.was_last_goal_reached_status_subscriber = rospy.Subscriber('/status/goal_reached', Bool,
+                                                                        self.was_last_goal_reached_status_callback)
 
     # --------------------------------------------
     # Helper functions
@@ -179,35 +142,26 @@ if __name__ == '__main__':
 
             # check that the thyroid is in the image
             if not controller.thyroid_in_image_status:
-                rospy.shutdown()
+                procedure_complete_flag = True
 
-            # change states to center the thyroid in the image
-            previous_procedure_state = procedure_state
-            procedure_state = CENTER_IMAGE
+            else:
+                print("pass")
+                # change states to center the thyroid in the image
+                previous_procedure_state = procedure_state
+                procedure_state = CENTER_IMAGE
 
         if procedure_state == CENTER_IMAGE:
 
-            # move the robot to center the centroid within the allowable error
-            if abs(controller.image_centroid_error_history[0]) > controller.allowable_centroid_error:
+            # if the image is not centered
+            if not controller.is_thyroid_centered:
 
-                # calculate required control input
-                centroid_control_inputs = controller.image_error_calculate_control_input()
-
-                # calculate force control inputs
-                force_control_inputs = controller.force_error_calculate_control_input()
-
-                # generate a message to use to send control inputs
-                centroid_correction_velocity = TwistStamped()
-
-                # assign values to the message
-                centroid_correction_velocity.twist.linear.x = centroid_control_inputs[0] + force_control_inputs[0]
-                centroid_correction_velocity.twist.linear.y = centroid_control_inputs[1] + force_control_inputs[1]
-                centroid_correction_velocity.twist.linear.z = centroid_control_inputs[2] + force_control_inputs[2]
-
-                # publish the message
-                controller.velocity_publisher.publish(centroid_correction_velocity)
+                # Publish that the image needs to be centered
+                controller.active_image_centering_status_publisher.publish(Bool(True))
 
             else:
+
+                # Publish that active image centering is no longer needed
+                controller.active_image_centering_status_publisher.publish(Bool(False))
 
                 # if the list of procedure_waypoints list is empty, save the current position as the origin
                 if len(procedure_waypoints) == 0:
@@ -217,9 +171,9 @@ if __name__ == '__main__':
                 if current_direction == DIRECTION_HEAD:
                     placement_index = -1
                 elif current_direction == DIRECTION_TORSO:
-                    placement_index == 0
+                    placement_index = 0
                 else:
-                    placement_index == 0
+                    placement_index = 0
 
                 # save the current position as a path waypoint to follow in the future
                 procedure_waypoints.insert(placement_index, controller.cartesian_position_history[0][1])
@@ -232,7 +186,6 @@ if __name__ == '__main__':
 
             # if the current objective of the procedure is to find waypoints,
             # set goal point as offset from current position
-
             if current_objective == WAYPOINT_FINDING:
 
                 # set direction of offset based on current direction of motion
@@ -264,26 +217,24 @@ if __name__ == '__main__':
 
         if procedure_state == MOVE_TO_GOAL:
 
-            if sqrt(sum(controller.move_goal - controller.cartesian_position_history[
-                0] ** 2)) > controller.acceptable_cartesian_error:
+            # if the current goal has not been reached
+            if not controller.was_last_goal_position_reached:
 
-                # create a message to send control velocities
-                velocity = TwistStamped()
+                # generate a new pose message
+                new_pose = TwistStamped()
 
-                # calculate positional control inputs
-                positional_control_inputs = controller.cartesian_position_calculate_control_input()
+                # fill out message with goal point
+                new_pose.twist.linear.x = controller.move_goal[0]
+                new_pose.twist.linear.y = controller.move_goal[1]
+                new_pose.twist.linear.z = controller.move_goal[2]
 
-                # calculate force control inputs = calculate_force_control_inputs
-                force_control_inputs = controller.force_error_calculate_control_input()
-
-                velocity.twist.linear.x = positional_control_inputs[0] + force_control_inputs[0]
-                velocity.twist.linear.y = positional_control_inputs[1] + force_control_inputs[1]
-                velocity.twist.linear.z = positional_control_inputs[2] + force_control_inputs[2]
+                # Publish the goal pose
+                controller.goal_pose_publisher.publish(TwistStamped())
 
             else:
 
-                # create an empty message
-                velocity = TwistStamped()
+                # Publish a message telling the robot to stop moving
+                controller.stop_motion_publisher.publish(Bool(True))
 
                 # save the previous state
                 previous_procedure_state = previous_procedure_state
@@ -298,10 +249,7 @@ if __name__ == '__main__':
 
                 # if scanning for images
                 if current_objective == SCANNING:
-                    procedure_state == SAVE_IMAGE
-
-            # Publish the velocity message
-            controller.velocity_publisher.publish(velocity)
+                    procedure_state = SAVE_IMAGE
 
         if procedure_state == CHECK_FOR_THYROID:
 
@@ -346,6 +294,3 @@ if __name__ == '__main__':
 
         if procedure_state == EXIT_PROCEDURE:
             procedure_complete_flag = True
-
-    velocity = TwistStamped()
-    controller.velocity_publisher.publish(velocity)

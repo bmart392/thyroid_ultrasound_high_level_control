d5d4e5a4db363b423477299a6ae5f33086f94633
Ben Mart <bmart392@gmail.com>
2024-04-18 14:40:22 -0400

    daily commit

--- scripts/Graphing/graphing_poseTest.py
+++ scripts/Graphing/graphing_poseTest.py
@@ -1,17 +1,28 @@
 # Import standard python packages
-from csv import DictReader
-from matplotlib.pyplot import show, subplots, rc, figure
-from numpy import array, rad2deg
-from statistics import median, stdev, mean
+from csv import DictReader, DictWriter
+from matplotlib.pyplot import show, subplots, rc
+from numpy import array
+from statistics import stdev, mean
 
 # Import custom python packages
-from ExperimentalDataRecorder import MESSAGE_ID, STAMP_SECS, STAMP_NSECS, FORCE, \
-    POSE_X, POSE_Y, POSE_Z, POSE_ROLL, POSE_PITCH, POSE_YAW, WAYPOINT_REACHED, SKIN_ERROR
-from Graphing.read_recorded_data_csv import read_recorded_data_csv, COMBINED_STAMP, CONTROLLED, UNCONTROLLED,\
+from ExperimentalDataRecorder import MESSAGE_ID, STAMP_SECS, STAMP_NSECS, \
+    POSE_X, POSE_Y, POSE_Z, POSE_ROLL, POSE_PITCH, POSE_YAW, WAYPOINT_REACHED
+from Graphing.read_recorded_data_csv import read_recorded_data_csv, COMBINED_STAMP, CONTROLLED, UNCONTROLLED, \
     FIG_WIDTH, FIG_HEIGHT
 from thyroid_ultrasound_robot_control_support.Helpers.calc_inverse import calc_inverse
-from thyroid_ultrasound_robot_control_support.Helpers.calc_straight_line_distance import calc_straight_line_distance
 from thyroid_ultrasound_robot_control_support.Helpers.calc_transformation_from_rpy import calc_transformation_from_rpy
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Define the timestamps of the files to use as sources
+time_stamps = ['2024-03-09_18-36-25-368610',
+               '2024-03-09_18-58-57-675893',
+               '2024-03-09_19-00-06-682067']
+
+# Select which file to plot
+file_index_to_plot = 2
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 # Set the default size for plot elements
 rc('font', size=10)  # controls default text sizes
@@ -23,202 +34,174 @@ rc('legend', fontsize=10)  # legend font size
 rc('figure', titlesize=24)  # font size of the figure title
 rc('figure', dpi=300)  # Set the DPI of the figure
 
-# Define helpful constants
-PROBE_X_DISTANCE: str = 'Probe X Position w.r.t.\nStarting Pose (m)'
-PROBE_Y_DISTANCE: str = 'Probe Y Position w.r.t.\nStarting Pose (m)'
-PROBE_Z_DISTANCE: str = 'Probe Z Position w.r.t.\nStarting Pose (m)'
-STRAIGHT_LINE_DISTANCE: str = 'Straight Line Distance (m)'
-COMBINED_STAMP: str = 'Elapsed Time (s)'
-
-# Define the source of the information
-time_stamps = ['2024-02-19_13-29-32-391129']
-line_colors = []
-
-time_stamp = '2024-03-09_18-36-25-368610'
-POSE_FILE_PATH: str = '/home/ben/thyroid_ultrasound_data/experimentation/PoseControl' \
-                      '/' + time_stamp + '_experiment/Pose_' + time_stamp + '.csv'
-
 # Create a figure and axes to plot the results
-fig = figure()
-axes_grid_space = fig.add_gridspec(3, 2, wspace=1.)
+fig, axes = subplots(nrows=1, ncols=1, figsize=(FIG_WIDTH, FIG_HEIGHT))
 
-# Define a counter to determine which plot to plot on
-axis_counter = 0
+# ----------------------------------------------------------------------------------------------------------------------
 
-# Title the figure
-# fig.suptitle('Force Control Experiment')
+# Define helpful constants
+PROBE_X_DISTANCE: str = 'Probe X Distance w.r.t.\nStarting Pose (m)'
+PROBE_Y_DISTANCE: str = 'Probe Y Distance w.r.t.\nStarting Pose (m)'
+PROBE_Z_DISTANCE: str = 'Probe Z Distance w.r.t.\nStarting Pose (m)'
 
-# Define a temporary dictionary to store each result
-temp_results_dict = {MESSAGE_ID: [], COMBINED_STAMP: [], POSE_X: [], POSE_Y: [], POSE_Z: [],
-                     POSE_ROLL: [], POSE_PITCH: [], POSE_YAW: [], WAYPOINT_REACHED: []}
+# Define constants to use in the results file
+DESIRED_DISTANCE: str = 'Desired Distance (mm)'
+DISTANCE_MEAN: str = 'Mean Distance (mm)'
+DISTANCE_STDEV: str = 'Standard Deviation of Distance (mm)'
+PITCH_ERROR: str = 'Error of Pose - Pitch (deg)'
+PITCH_STDEV: str = 'Standard Deviation of Error of Pose - Pitch (deg)'
+YAW_ERROR: str = 'Error of Pose - Yaw (deg)'
+YAW_STDEV: str = 'Standard Deviation of Error of Pose - Yaw (deg)'
+NUMBER_OF_DATA_POINTS: str = 'Number of Data Points'
+
+# ----------------------------------------------------------------------------------------------------------------------
 # Define final dictionary to store each result
-final_results_dict = {MESSAGE_ID: [], COMBINED_STAMP: [], POSE_X: [], POSE_Y: [], POSE_Z: [],
-                      POSE_ROLL: [], POSE_PITCH: [], POSE_YAW: [],
-                      STRAIGHT_LINE_DISTANCE: [], PROBE_X_DISTANCE: [], PROBE_Y_DISTANCE: [], PROBE_Z_DISTANCE: []}
-waypoints = {COMBINED_STAMP: [], POSE_X: [], POSE_PITCH: [], POSE_YAW: []}
-# Open the file
-source_file = open(POSE_FILE_PATH, mode='r')
-source_file_reader = DictReader(source_file)
+final_results_dict = {COMBINED_STAMP: [], PROBE_X_DISTANCE: [], PROBE_Y_DISTANCE: [], PROBE_Z_DISTANCE: [],
+                      WAYPOINT_REACHED + COMBINED_STAMP: [], WAYPOINT_REACHED + POSE_X: []}
 
-# Create variables to store the starting pose of the test
-first_pass = True
-starting_pose = array([])
+# Define a flag to note that it is the first data file
+is_first_data_file = True
 
-# Read in the data
-for row in source_file_reader:
+# Open the results file
+results_file = open('/home/ben/thyroid_ultrasound_data/experimentation/PoseControl/results.csv', mode='w')
+results_file_writer = DictWriter(results_file, delimiter=',',
+                                 fieldnames=[DESIRED_DISTANCE, DISTANCE_MEAN, DISTANCE_STDEV,
+                                             PITCH_ERROR, PITCH_STDEV, YAW_ERROR, YAW_STDEV,
+                                             NUMBER_OF_DATA_POINTS])
+results_file_writer.writeheader()
 
-    # Capture the starting pose if it is the first time through the loop
-    if first_pass:
-        starting_pose = calc_transformation_from_rpy(xyz=(float(row[POSE_X]), float(row[POSE_Y]), float(row[POSE_Z])),
-                                                     rpy=(float(row[POSE_ROLL]), float(row[POSE_PITCH]), float(row[POSE_YAW])))
-        first_pass = False
-    # Pull the data from the test
-    temp_results_dict[MESSAGE_ID].append(int(row[MESSAGE_ID]))
-    temp_results_dict[COMBINED_STAMP].append((int(row[STAMP_SECS])) + (int(row[STAMP_NSECS])) / 10 ** 9)
-    temp_results_dict[POSE_X].append(float(row[POSE_X]))
-    temp_results_dict[POSE_Y].append(float(row[POSE_Y]))
-    temp_results_dict[POSE_Z].append(float(row[POSE_Z]))
-    temp_results_dict[POSE_ROLL].append(float(row[POSE_ROLL]))
-    temp_results_dict[POSE_PITCH].append(float(row[POSE_PITCH]))
-    temp_results_dict[POSE_YAW].append(float(row[POSE_YAW]))
-    temp_results_dict[WAYPOINT_REACHED].append(bool(int(row[WAYPOINT_REACHED])))
+# ----------------------------------------------------------------------------------------------------------------------
 
-# Find the smallest values from the data
-min_stamp = min(temp_results_dict[COMBINED_STAMP])
+for file_index, time_stamp in enumerate(time_stamps):
 
-# Calculate the inverse matrix of the starting pose
-inverse_starting_pose = calc_inverse(starting_pose)
+    pose_file_path = '/home/ben/thyroid_ultrasound_data/experimentation/PoseControl' \
+                     '/' + time_stamp + '_experiment/Pose_' + time_stamp + '.csv'
 
-# Improve the data for graphing
-for message_id, seconds_stamp, pose_x, pose_y, pose_z, pose_roll, pose_pitch, pose_yaw, waypoint_reached in zip(
-        temp_results_dict[MESSAGE_ID],
-        temp_results_dict[COMBINED_STAMP],
-        temp_results_dict[POSE_X],
-        temp_results_dict[POSE_Y],
-        temp_results_dict[POSE_Z],
-        temp_results_dict[POSE_ROLL],
-        temp_results_dict[POSE_PITCH],
-        temp_results_dict[POSE_YAW],
-        temp_results_dict[WAYPOINT_REACHED]):
-    # Pull the data from the previous array
-    final_results_dict[MESSAGE_ID].append(message_id)
-    final_results_dict[COMBINED_STAMP].append(seconds_stamp - min_stamp)
-    final_results_dict[POSE_X].append(pose_x)
-    final_results_dict[POSE_Y].append(pose_y)
-    final_results_dict[POSE_Z].append(pose_z)
-    final_results_dict[POSE_ROLL].append(pose_roll)
-    final_results_dict[POSE_PITCH].append(pose_pitch)
-    final_results_dict[POSE_YAW].append(pose_yaw)
-    final_results_dict[STRAIGHT_LINE_DISTANCE].append(calc_straight_line_distance((starting_pose[0][3],
-                                                                              starting_pose[1][3],
-                                                                              starting_pose[2][3]),
-                                                                             (pose_x, pose_y, pose_z)))
+    # Determine if the data from this file should be saved
+    save_data_to_plot = file_index == file_index_to_plot
 
-    # Convert the current pose to a transformation matrix
-    current_pose = calc_transformation_from_rpy(xyz=(pose_x, pose_y, pose_z),
-                                                rpy=(-pose_roll, pose_pitch, pose_yaw))
+    # Define a dictionary in which to store the result data
+    waypoint_data = {PROBE_X_DISTANCE: [], POSE_PITCH: [], POSE_YAW: []}
 
-    # Calculate the current pose in terms of the starting pose
-    position = inverse_starting_pose @ array([[current_pose[0, 3]],
-                                              [current_pose[1, 3]],
-                                              [current_pose[2, 3]],
-                                              [1]])
+    # Define variables to store information about the starting pose
+    starting_pose = None
+    inverse_starting_pose = None
+    previous_distance = None
+    pitch_at_start = None
+    yaw_at_start = None
 
-    # Pull out the distance in X from the starting pose
-    final_results_dict[PROBE_X_DISTANCE].append(abs(position[0][0]))
-    final_results_dict[PROBE_Y_DISTANCE].append(abs(position[1][0]))
-    final_results_dict[PROBE_Z_DISTANCE].append(abs(position[2][0]))
+    # Read in the data from the source file
+    data = read_recorded_data_csv(file_path=pose_file_path,
+                                  headings=[MESSAGE_ID, STAMP_SECS, STAMP_NSECS,
+                                            POSE_X, POSE_Y, POSE_Z,
+                                            POSE_ROLL, POSE_PITCH, POSE_YAW,
+                                            WAYPOINT_REACHED],
+                                  sort_heading=MESSAGE_ID,
+                                  additional_headings_to_zero=[STAMP_SECS])
 
-    if waypoint_reached:
-        waypoints[COMBINED_STAMP].append(seconds_stamp - min_stamp)
-        waypoints[POSE_X].append(abs(position[0][0]))
-        waypoints[POSE_PITCH].append(pose_pitch)
-        waypoints[POSE_YAW].append(pose_yaw)
+    # For each data point,
+    for i in range(len(data[WAYPOINT_REACHED])):
 
-distance_between_waypoints = [waypoints[POSE_X][ii] - waypoints[POSE_X][ii - 1] for ii in range(2, len(waypoints[POSE_X]))]
-clean_distance_between_waypoints = []
-for distance in distance_between_waypoints:
-    if abs(distance) > 10 ** -4:
-        clean_distance_between_waypoints.append(distance)
+        # Convert the current pose to a transformation matrix
+        this_pose = calc_transformation_from_rpy(xyz=(data[POSE_X][i], data[POSE_Y][i], data[POSE_Z][i]),
+                                                 rpy=(-data[POSE_ROLL][i], data[POSE_PITCH][i], data[POSE_YAW][i]))
 
-print("Distance between waypoints")
-print("Mean: " + str(mean(clean_distance_between_waypoints)))
-print("Median: " + str(median(clean_distance_between_waypoints)))
-print("Std Dev: " + str(stdev(clean_distance_between_waypoints)))
+        try:
+            # Calculate the current pose in terms of the starting pose
+            position = inverse_starting_pose @ array([[this_pose[0, 3]], [this_pose[1, 3]], [this_pose[2, 3]], [1]])
 
+            # Only add data to be graphed if it is the first data file
+            if save_data_to_plot:
+                # Update the final results dictionary
+                final_results_dict[COMBINED_STAMP].append(data[COMBINED_STAMP][i])
+                final_results_dict[PROBE_X_DISTANCE].append(abs(position[0][0]))
+                final_results_dict[PROBE_Y_DISTANCE].append(abs(position[1][0]))
+                final_results_dict[PROBE_Z_DISTANCE].append(abs(position[2][0]))
 
-print("Pose Pitch Desired: " + str(final_results_dict[POSE_PITCH][0]))
-print("Mean: " + str(mean(waypoints[POSE_PITCH])))
-print("Median: " + str(median(waypoints[POSE_PITCH])))
-print("Std Dev: " + str(stdev(waypoints[POSE_PITCH])))
+        except ValueError:
+            position = None
 
-print("Pose Yaw Desired: " + str(final_results_dict[POSE_YAW][0]))
-print("Mean: " + str(mean(waypoints[POSE_YAW])))
-print("Median: " + str(median(waypoints[POSE_YAW])))
-print("Std Dev: " + str(stdev(waypoints[POSE_YAW])))
+        if data[WAYPOINT_REACHED][i]:
 
+            # If the starting pose is known,
+            try:
+
+                # Find the magnitude of the x distance
+                x_distance = abs(position[0][0])
+
+                if abs(x_distance - previous_distance) > 10 ** -5:
+
+                    # Add the relevant data to the final results dictionary
+                    if save_data_to_plot:
+                        final_results_dict[WAYPOINT_REACHED + COMBINED_STAMP].append(data[COMBINED_STAMP][i])
+                        final_results_dict[WAYPOINT_REACHED + POSE_X].append(x_distance)
+
+                    # Add the data to the waypoints dictionary
+                    waypoint_data[PROBE_X_DISTANCE].append(x_distance - previous_distance)
+                    waypoint_data[POSE_PITCH].append(data[POSE_PITCH][i] - pitch_at_start)
+                    waypoint_data[POSE_YAW].append(data[POSE_YAW][i] - yaw_at_start)
+
+                    # Update the previous distance
+                    previous_distance = x_distance
+
+            # Otherwise,
+            except TypeError:
+                # Define the starting pose
+                starting_pose = calc_transformation_from_rpy(
+                    xyz=(data[POSE_X][i], data[POSE_Y][i], data[POSE_Z][i]),
+                    rpy=(-data[POSE_ROLL][i], data[POSE_PITCH][i], data[POSE_YAW][i]))
+
+                # Calculate the inverse matrix of the starting pose
+                inverse_starting_pose = calc_inverse(starting_pose)
+
+                # Save the values of the first position
+                previous_distance = 0
+                pitch_at_start = data[POSE_PITCH][i]
+                yaw_at_start = data[POSE_YAW][i]
+
+    # Calculate the mean distance for convenience
+    mean_distance_between_waypoints = mean(waypoint_data[PROBE_X_DISTANCE]) * 10 ** 3
+
+    # Write the data into the results file
+    results_file_writer.writerow({DESIRED_DISTANCE: round(mean_distance_between_waypoints, 0),
+                                  DISTANCE_MEAN: round(mean_distance_between_waypoints, 3),
+                                  DISTANCE_STDEV: round(stdev(waypoint_data[PROBE_X_DISTANCE]) * 10 ** 3, 3),
+                                  PITCH_ERROR: round(mean(waypoint_data[POSE_PITCH]), 3),
+                                  PITCH_STDEV: round(stdev(waypoint_data[POSE_PITCH]), 3),
+                                  YAW_ERROR: round(mean(waypoint_data[POSE_YAW]), 3),
+                                  YAW_STDEV: round(stdev(waypoint_data[POSE_YAW]), 3),
+                                  NUMBER_OF_DATA_POINTS: len(waypoint_data[PROBE_X_DISTANCE])})
+
+# Close the results file
+results_file.close()
 
 # Define the sampling rate for the points included in the plot
-point_sampling_rate = 2
+point_sampling_rate = 10
 
-# Define variables for convenience
+# Define the common X axis for all the data
 common_x = final_results_dict[COMBINED_STAMP][::point_sampling_rate]
 
-# axes.set_title("No Force Control")
-# ax_0 = fig.add_subplot(axes_grid_space[0, 0:2])
-# ax_0.scatter(common_x, final_results_dict[POSE_X][::point_sampling_rate], s=0.5, c='r')
-# ax_0.set_xlabel(COMBINED_STAMP)
-# ax_0.set_ylabel('Probe X Position\nw.r.t. Origin (m)')
-# ax_0.grid(axis='y')
-#
-# ax_1 = fig.add_subplot(axes_grid_space[0, 2:4])
-# ax_1.scatter(common_x, final_results_dict[POSE_Y][::point_sampling_rate], s=0.5, c='r')
-# ax_1.set_xlabel(COMBINED_STAMP)
-# ax_1.set_ylabel('Probe Y Position\nw.r.t. Origin (m)')
-# ax_1.grid(axis='y')
-#
-# ax_2 = fig.add_subplot(axes_grid_space[0, 4:6])
-# ax_2.scatter(common_x, final_results_dict[POSE_Z][::point_sampling_rate], s=0.5, c='r')
-# ax_2.set_xlabel(COMBINED_STAMP)
-# ax_2.set_ylabel('Probe Z Position\nw.r.t. Origin (m)')
-# ax_2.grid(axis='y')
+# Plot the data for each axis as a line
+axes.plot(common_x, final_results_dict[PROBE_X_DISTANCE][::point_sampling_rate],
+          linewidth=1, color='red', label='X Axis')
+axes.plot(common_x, final_results_dict[PROBE_Y_DISTANCE][::point_sampling_rate],
+          linewidth=1, color='green', label='Y Axis')
+axes.plot(common_x, final_results_dict[PROBE_Z_DISTANCE][::point_sampling_rate],
+          linewidth=1, color='blue', label='Z Axis')
 
+# Add the legend to the axis
+axes.legend()
 
-# ax_3 = fig.add_subplot(axes_grid_space[1, 0:3])
-# ax_3.scatter(common_x, final_results_dict[STRAIGHT_LINE_DISTANCE][::point_sampling_rate], s=0.5, c='r')
-# ax_3.set_xlabel(COMBINED_STAMP)
-# ax_3.set_ylabel(STRAIGHT_LINE_DISTANCE)
-# ax_3.grid(axis='y')
+# Add markers for when each waypoint was reached
+axes.scatter(final_results_dict[WAYPOINT_REACHED + COMBINED_STAMP], final_results_dict[WAYPOINT_REACHED + POSE_X],
+             s=6, c='black', marker="D")
 
-ax_4 = fig.add_subplot(axes_grid_space[:, :])
-ax_4.scatter(common_x, final_results_dict[PROBE_X_DISTANCE][::point_sampling_rate],
-             s=0.5, c='red', label='X Axis')
-ax_4.scatter(common_x, final_results_dict[PROBE_Y_DISTANCE][::point_sampling_rate],
-             s=0.5, c='green', label='Y Axis')
-ax_4.scatter(common_x, final_results_dict[PROBE_Z_DISTANCE][::point_sampling_rate],
-             s=0.5, c='blue', label='Z Axis')
-ax_4.legend()
-ax_4.scatter(waypoints[COMBINED_STAMP], waypoints[POSE_X], s=6, c='black', marker="D")
-ax_4.set_xlabel(COMBINED_STAMP)
-ax_4.set_ylabel("Distance Travelled w.r.t. Initial Pose (m)")
-ax_4.grid(axis='y')
+# Set the axis labels
+axes.set_xlabel(COMBINED_STAMP)
+axes.set_ylabel("Distance Travelled w.r.t. Initial Pose (m)")
 
+# Set the grid
+axes.grid(axis='y')
 
-# axes[0][0].scatter(common_x, final_results_dict[POSE_X][::point_sampling_rate], s=0.5, c='r')
-# axes[0][0].set_xlabel(STAMP_SECS)
-# axes[0][0].set_ylabel('Robot Position w.r.t. Origin (m)')
-# axes[0][0].grid(axis='y')
-# axes[0][0].scatter(common_x, final_results_dict[POSE_Y][::point_sampling_rate], s=0.5, c='r')
-# axes[0][1].set_xlabel(STAMP_SECS)
-# axes[0][1].set_ylabel(POSE_Y)
-# axes[0][1].grid(axis='y')
-# axes[1][0].scatter(common_x, final_results_dict[POSE_Z][::point_sampling_rate], s=0.5, c='r')
-# axes[1][0].set_xlabel(STAMP_SECS)
-# axes[1][0].set_ylabel(POSE_Z)
-# axes[1][0].grid(axis='y')
-# axes[1][1].scatter(common_x, final_results_dict[PROBE_X_DISTANCE][::point_sampling_rate], s=0.5, c='r')
-# axes[1][1].set_xlabel(PROBE_X_DISTANCE)
-# axes[1][1].set_ylabel(POSE_Z)
-# axes[1][1].grid(axis='y')
-
+# Show the graph
 show()
